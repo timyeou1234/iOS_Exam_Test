9652ba1a4c0e1aa9230ce59c2529953689ea51d5
TimothyYeou <timyeou1234@hotmail.com>
2019-03-01 14:20:18 +0800

    Init push to start the project

---
+++ iOS_Exam_Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_Exam_Test
+//
+//  Created by YeouTimothy on 2019/3/1.
+//  Copyright © 2019 Timothy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ iOS_Exam_TestTests/iOS_Exam_TestTests.swift
@@ -0,0 +1,34 @@
+//
+//  iOS_Exam_TestTests.swift
+//  iOS_Exam_TestTests
+//
+//  Created by YeouTimothy on 2019/3/1.
+//  Copyright © 2019 Timothy. All rights reserved.
+//
+
+import XCTest
+@testable import iOS_Exam_Test
+
+class iOS_Exam_TestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
